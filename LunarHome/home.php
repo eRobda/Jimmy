42e68ac6b4a960025c2e2a6a3249b38ff43943d2
Robin Ustohal <51453874+eRobda@users.noreply.github.com>
2024-04-21 22:47:19 +0200

    Add files via upload

---
+++ LunarHome/home.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lunar Home</title>
+    <link rel="manifest" href="manifest.json">
+    <link rel="icon" href="icon/hd_hi.ico">
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body class="w-dvw h-dvh overflow-hidden fixed flex justify-center items-center">
+    <div class="w-1/2">
+        <img src="icon/logo-bez-sloganu.png" alt="">
+    </div>
+</body>
+<script>
+    setTimeout(() => {
+        window.location.href = "home.html";
+    }, 2000);
+</script>
+</html>
